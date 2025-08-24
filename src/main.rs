bbecb6667cf3583aab8d685fcc1183d2f2936a20
zenonet <git@zenonet.de>
2025-08-24 18:20:11 +0200

    Played around until i got to somewhat working looking boids

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use bevy::math::NormedVectorSpace;
 use bevy::prelude::*;
 use bevy::{app::{App, Startup}, asset::Assets, color::Color, core_pipeline::core_2d::Camera2d, ecs::{component::Component, system::{Commands, Query, ResMut}}, math::{primitives::Circle, Vec2, Vec3}, render::mesh::{Mesh, Mesh2d}, sprite::{ColorMaterial, MeshMaterial2d}, transform::components::Transform, DefaultPlugins};
+use rand::Rng;
 
 const TICK_RATE: f32 = 60.0;
 fn main() {
@@ -23,9 +25,17 @@ struct Velocity(Vec3);
 
 #[derive(Component, Clone, Copy)]
 struct ParticleComputationData{
-    center: Vec3
+    center: Vec3,
+    heading: Vec3,
+    avoidance_dir: Vec3,
 }
 
+
+fn rand_vec() -> Vec3{
+    let angle:f32 = rand::rng().random_range(0.0..360.0);
+    let s = angle.sin_cos();
+    Vec3::new(s.0, s.1, 0.0)
+}
 
 fn spawn_particles(
     mut commands: Commands,
@@ -36,26 +46,49 @@ fn spawn_particles(
 
 
     let particle_mesh = meshes.add(Circle::default());
+    let particle_mesh2 = meshes.add(Circle::default());
     let particle_material = materials.add(Color::srgb(255.0, 0.0, 0.0));
+    let particle_material2 = materials.add(Color::srgb(0.0, 255.0, 0.0));
 
     let radius = 75.0;
-    let cnt  = 50;
-    let particles = (0..50).map(move |i|{
+    let cnt  = 360;
+    let particles = (0..36).map(move |i|{
         let angle = i as f32 / cnt as f32 * 360.0;
         let (y, x) = angle.sin_cos();
-        let x = x*radius;
+        let x = x*radius -100.0;
         let y = y*radius;
         (
             Particle{},
-            Velocity(Vec3::ZERO),
+            Velocity(rand_vec()),
             ParticleComputationData{
-                center: Vec3::default()
+                center: Vec3::default(),
+                heading: Vec3::default(),
+                avoidance_dir: Vec3::default(),
             },
             Mesh2d(particle_mesh.clone()),
             MeshMaterial2d(particle_material.clone()),
             Transform::from_translation(Vec3::new(x, y, 1.0)).with_scale(Vec2::splat(5.0).extend(1.))
         )
     });
+
+    let particles = particles.chain((0..36).map(move |i|{
+        let angle = i as f32 / cnt as f32 * 360.0;
+        let (y, x) = angle.sin_cos();
+        let x = x*radius + 100.0;
+        let y = y*radius;
+        (
+            Particle{},
+            Velocity(rand_vec()),
+            ParticleComputationData{
+                center: Vec3::default(),
+                heading: Vec3::default(),
+                avoidance_dir: Vec3::default(),
+            },
+            Mesh2d(particle_mesh2.clone()),
+            MeshMaterial2d(particle_material2.clone()),
+            Transform::from_translation(Vec3::new(x, y, 1.0)).with_scale(Vec2::splat(5.0).extend(1.))
+        )
+    }));
 
     commands.spawn_batch(particles);
 }
@@ -70,30 +103,64 @@ fn apply_velocity(
 
 
 fn update_particle_data(
-    particles: Query<&mut ParticleComputationData>,
-    other_particles: Query<(&Particle, &Transform)>
+    particles: Query<(&mut ParticleComputationData, &Transform)>,
+    other_particles: Query<(&Particle, &Velocity, &Transform)>
 ){
-    for mut data in particles{
+    for (mut data, transform) in particles{
 
         let mut count = 0;
+        let mut proximity_count = 0;
         let mut pos = Vec3::default();
-        for (p, transform) in other_particles {
-            pos += transform.translation;
+        let mut heading = Vec3::default();
 
+        let mut avoidance_dir = Vec3::default();
+        let mut avoidance_count = 0;
+        for (p, velocity, trans) in other_particles {
+            let distance = transform.translation.distance(trans.translation);
+
+            if distance > 75.0{
+                continue;
+            }
+            pos += trans.translation;
             count += 1;
+
+            heading += velocity.0.normalize_or_zero();
+            proximity_count += 1;
+
+            if distance < 20.0{
+                avoidance_dir += (transform.translation - trans.translation).normalize_or_zero();
+                avoidance_count += 1;
+            }
         }
 
-        data.center = pos * (1.0 / count as f32);
+        data.center = (pos * (1.0 / count as f32));
+        data.heading = (heading * (proximity_count as f32).recip()).normalize_or_zero();
+        data.avoidance_dir = avoidance_dir * (avoidance_count as f32).recip();
     }
 }
 
 fn update_particles(
     particles: Query<(&mut Particle, &mut Velocity, &Transform, &ParticleComputationData)>
 ){
+    let mut avoidance_counter = 0;
     for (mut p, mut vel, transform, data) in particles{
-        let direction = (data.center - transform.translation).normalize();
 
-        let acceleration: f32 = 0.5;
+        let fixed_center_cohesion = -transform.translation.normalize_or_zero();
+
+        let cohesion = (data.center - transform.translation).normalize_or_zero();
+
+        let avoidance = data.avoidance_dir.normalize_or_zero();
+        if avoidance.length_squared() == 0.0{
+            avoidance_counter += 1;
+        }
+        let direction = (0.3* cohesion + 1.0*data.heading + 2.0* avoidance + 0.7 * fixed_center_cohesion);
+
+        let direction = direction.normalize();
+
+        let acceleration: f32 = 5.0;
+        let max_speed:f32 = 100.0;
         vel.0 += direction * acceleration;
+
+        vel.0 = vel.0.clamp_length_max(max_speed);
     }
 }
