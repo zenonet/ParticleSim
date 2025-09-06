c5f3754492623d726ae0d14222fc487c8c5e1919
zenonet <git@zenonet.de>
2025-09-06 17:06:53 +0200

    Implemented a macro allowing to match multiple tokens from a tokenstream

--- src/particlescript/parser.rs
+++ src/particlescript/parser.rs
@@ -1,41 +1,8 @@
-use std::{rc::Rc, sync::atomic::AtomicUsize};
+use std::rc::Rc;
 
-use crate::particlescript::{lexer::{Token, TokenType}, types::Type};
-use bevy::{render::render_resource::encase::private::Length, tasks::futures_lite::StreamExt, text::cosmic_text::ttf_parser::morx::Chain};
-use itertools::{Itertools, MultiPeek};
+use crate::particlescript::{lexer::Token, types::Type};
+use itertools::Itertools;
 
-
-// pub struct Parser<T>
-// where T: Iterator<Item = Token>{
-//    tokens: T
-// }
-
-// impl<T:Iterator<Item = Token>> Parser<T>{
-    
-// }
-
-
-// trait PeekSlice<T, I>
-// where T: Iterator<Item = I>{
-//     fn peek<'a>(&'a mut self, length: usize) -> Vec<&'a I>;    
-// }
-
-// impl<T: Iterator<Item = I>, I> PeekSlice<T, I> for MultiPeek<T>{
-//     fn peek<'a>(&'a mut self, length: usize) -> Vec<&'a I> {
-
-//         let mut v = Vec::<&I>::with_capacity(length);
-
-//         self
-//                     v.push(self.peek().unwrap());
-//                                 v.push(self.peek().unwrap());
-
-
-//         for i in 0..length{
-//         }
-
-//         v
-//     }
-// }
 
 
 #[derive(Debug)]
@@ -67,45 +34,6 @@ enum Stmt{
 }
 
 
-
-trait ChainElement{
-    fn append<const LENGTH:usize>(self, new: StmtDef<LENGTH>) -> Definition<LENGTH, Self>
-    where Self: Sized;
-}
-struct StmtDef<const LENGTH: usize>{
-    pub pattern: [TokenType; LENGTH],
-}
-impl<const LENGTH:usize> StmtDef<LENGTH>{
-    pub fn new(pattern: [TokenType; LENGTH]) -> Self{
-        Self { pattern }
-    }
-}
-
-
-struct StmtDefinitionChain;
-
-impl ChainElement for StmtDefinitionChain{
-    fn append<const LENGTH: usize>(self, new: StmtDef<LENGTH>) -> Definition<LENGTH, Self>
-    where Self: Sized {
-        Definition { def: new, next: Self }
-    }
-}
-
-/// The end of the chain
-struct Definition<const LENGTH:usize, Next>
-where Next: ChainElement{
-    pub def: StmtDef<LENGTH>,
-    pub next: Next
-}
-impl<const LENGTH:usize, Next: ChainElement> ChainElement for Definition<LENGTH, Next>{
-    fn append<const n:usize>(self, new: StmtDef<{n}>) -> Definition<n, Self>{
-        Definition { 
-            def: new,
-            next: self 
-        }
-    }
-}
-
 fn parse<T>(
     tokens: T,
     scope: &mut Scope
@@ -116,14 +44,57 @@ where T: Iterator<Item = Token>{
     let t = tokens.next().unwrap();
 }
 
+macro_rules! match_tokens {
+    ( $tokens:expr, $( $x:pat_param ),* ) => {
+        {
+            (move ||{
+        $( 
+            let Some(t) = $tokens.peek() else {return false;};
+            match t.token_type{
+                $x => {},
+                _ => {return false;}
+            }
+        )*
+        return true;
+    })()
+    }
+    };
+}
+
 #[cfg(test)]
 mod test{
-    use crate::particlescript::{lexer::TokenType, parser::{ChainElement, Definition, StmtDef, StmtDefinitionChain}};
+    use itertools::Itertools;
+
+    use crate::particlescript::{lexer::{Token, TokenType}}
 
     #[test]
     fn defs(){
-        let defs = StmtDefinitionChain{}
-            .append(StmtDef::new([TokenType::Let]))
-            .append(StmtDef::new([TokenType::For, TokenType::OpeningParenthesis]));
+        let tokens = [            
+            Token{
+                token_type: TokenType::Identifier("ln".to_owned()),
+                line: 1,
+                column: 1
+            },
+            Token{
+                token_type: TokenType::OpeningParenthesis,
+                line: 1,
+                column: 5
+            },
+            Token{
+                token_type: TokenType::ClosingParenthesis,
+                line: 3,
+                column: 2
+            }
+        ];
+        let mut tokens = tokens.iter().multipeek();
+
+        let res:bool = match_tokens!(
+            tokens,
+            TokenType::Identifier(_),
+            TokenType::OpeningParenthesis,
+            TokenType::ClosingParenthesis
+        );
+
+        assert!(res)
     }
 }
