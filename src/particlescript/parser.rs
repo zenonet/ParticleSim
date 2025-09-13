427f689b03ba639693d07c650fec0723dc553fe7
zenonet <git@zenonet.de>
2025-09-13 16:32:46 +0200

    semi-working parser macro

--- src/particlescript/parser.rs
+++ src/particlescript/parser.rs
@@ -1,9 +1,37 @@
 use std::rc::Rc;
 
-use crate::particlescript::{lexer::Token, types::Type};
+use crate::particlescript::{lexer::Token, lexer::TokenType::*, types::Type};
 use itertools::Itertools;
 
 
+macro_rules! match_tokens {
+    ( $tokens:expr, $( $x:pat_param ),* ) => {
+        {
+            //let tokens: itertools::MultiPeek<std::slice::IntoIter<'_, Token>> = $tokens;
+            (move ||{
+                let mut cnt = 0;
+        $( 
+            let Some(t) = $tokens.peek() else {return None;};
+            match t.token_type{
+                $x => {cnt += 1},
+                _ => {return None;}
+            }
+        )*
+
+        // Let's collect the tokens
+        let mut res = Vec::<Token>::with_capacity(cnt);
+
+        $(
+            // We gotta use $x somewhere, let's just hope the compiler optimizes this away
+            matches!(TokenType::Semicolon, $x);
+            res.push($tokens.next().unwrap());
+        )*
+        
+        return Some(res);
+    })()
+    }
+    };
+}
 
 #[derive(Debug)]
 struct Variable{
@@ -41,37 +69,15 @@ fn parse<T>(
 where T: Iterator<Item = Token>{
     let mut tokens = tokens.multipeek();
 
-    let t = tokens.next().unwrap();
-}
-//#![feature(macro_metavar_expr)]
-macro_rules! match_tokens {
-    ( $tokens:expr, $( $x:pat_param ),* ) => {
-        {
-            //let tokens: itertools::MultiPeek<std::slice::IntoIter<'_, Token>> = $tokens;
-            (move ||{
-                let mut cnt = 0;
-        $( 
-            let Some(t) = $tokens.peek() else {return None;};
-            match t.token_type{
-                $x => {cnt += 1},
-                _ => {return None;}
-            }
-        )*
-
-        // Let's collect the tokens
-        let mut res = Vec::<Token>::with_capacity(cnt);
-
-        $(
-            // We gotta use $x somewhere, let's just hope the compiler optimizes this away
-            matches!(TokenType::Semicolon, $x);
-            res.push($tokens.next().unwrap());
-        )*
-        
-        return Some(res);
-    })()
+    if let Some(tok) = match_tokens!(tokens,
+        Identifier(name),
+        Equals
+    ){
+        println!("{}", name);
     }
-    };
 }
+
+
 
 #[cfg(test)]
 mod test{
